210bed7fa486d049fc2a89c976fcd137f5852e28
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2025-01-13 06:44:13 +0100

    Update pwa.js

--- pwa.js
+++ pwa.js
@@ -1,11 +1,40 @@
 import React from "https://esm.sh/react@19/?dev"
 import ReactDOMClient from "https://esm.sh/react-dom@19/client?dev"
 
-var Header = () => <div id='Header'>... Header ...</div>;
-var MainArea = () => <div id='MainArea'>... MainArea ...</div>;
+//========= Register Service Worker =========
+
+window.onload = () => {
+  'use strict';
+  if( 'serviceWorker' in navigator ){
+    navigator.serviceWorker.register('sw.js');
+  }
+}
+
+/////////////// THE PWA ///////////////
+
+function Options(){
+  return (<div id='Options'>
+            Options
+          </div>)
+}
+function Patterns(){
+  return (<div id='Options'>
+            Patterns
+          </div>)
+}
+function Preview(){
+  return (<div id='Options'>
+            Preview
+          </div>)
+}
+function Code(){
+  return (<div id='Code'>
+            Code
+          </div>)
+}
 
 // Render
-var rootElement = <div id='root'><Header/><MainArea/></div>;
+var r = <><Options/><Patterns/><Preview/><Code/></>;
 ReactDOMClient.createRoot( 
   document.getElementById('app') 
-).render(rootElement);
+).render(r);
