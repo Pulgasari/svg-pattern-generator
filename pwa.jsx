45e8a5e7a187467cbc8092c76dcadc065b9defed
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2025-01-17 02:46:49 +0100

    update

--- pwa.jsx
+++ pwa.jsx
@@ -375,6 +375,12 @@ const PATTERNS_SOURCE = [
      "width": "100",
        "svg": "<svg><g fill='#000000'><path d='M0 199V0h1v1.99L100 199h-1.12L1 4.22V199H0zM100 2h-.12l-1-2H100v2z'></path></g></svg>",
   },
+  {
+      "name": "autumn",
+    "height": "24",
+     "width": "88",
+       "svg": "<svg><g fill-rule='evenodd'><g id='autumn' fill='#000000'><path d='M10 0l30 15 2 1V2.18A10 10 0 0 0 41.76 0H39.7a8 8 0 0 1 .3 2.18v10.58L14.47 0H10zm31.76 24a10 10 0 0 0-5.29-6.76L4 1 2 0v13.82a10 10 0 0 0 5.53 8.94L10 24h4.47l-6.05-3.02A8 8 0 0 1 4 13.82V3.24l31.58 15.78A8 8 0 0 1 39.7 24h2.06zM78 24l2.47-1.24A10 10 0 0 0 86 13.82V0l-2 1-32.47 16.24A10 10 0 0 0 46.24 24h2.06a8 8 0 0 1 4.12-4.98L84 3.24v10.58a8 8 0 0 1-4.42 7.16L73.53 24H78zm0-24L48 15l-2 1V2.18A10 10 0 0 1 46.24 0h2.06a8 8 0 0 0-.3 2.18v10.58L73.53 0H78z'/></g></g></svg>",
+  },
 ];
 
 /////////////// Atoms (Global States) ///////////////
@@ -742,7 +748,7 @@ function Preview(){
   let          fgColor = useAtomValue( fgColorAtom     ); console.log(     'fgColor:',     fgColor );
   let        fgOpacity = useAtomValue( fgOpacityAtom   ); console.log(   'fgOpacity:',   fgOpacity );
   let      patternName = useAtomValue( patternNameAtom ); console.log( 'patternName:', patternName );
-  let    patternObject = PATTERNS_SOURCE.find( pattern => pattern.name === patternName )
+  let    patternObject = PATTERNS_SOURCE.find( pattern => pattern.name === patternName ) || {};
   let patternSvgString = getPatternSvgString( patternObject, { fgColor, fgOpacity } );
   console.log(patternObject);
   console.log(patternSvgString);
