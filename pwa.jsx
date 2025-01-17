4cdeeaa3d7093f26cf5645efde2511f5d7595914
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2025-01-17 06:04:38 +0100

    update

--- pwa.jsx
+++ pwa.jsx
@@ -468,6 +468,21 @@ const PATTERNS_SOURCE = [
    "width": "16",
      "svg": "<svg><g fill='#000000'><polygon points='8 -4.4408921e-16 8 20 0 10'></polygon><polygon points='16 0 16 10 8 0'></polygon><polygon points='16 10 16 20 8 20'></polygon></g></svg>",
   },{
+    "name": "heavy-rain",
+  "height": "24",
+   "width": "12",
+     "svg": "<svg><path fill='#000000' d='M2 0h2v12H2V0zm1 20c1.105 0 2-.895 2-2s-.895-2-2-2-2 .895-2 2 .895 2 2 2zM9 8c1.105 0 2-.895 2-2s-.895-2-2-2-2 .895-2 2 .895 2 2 2zm-1 4h2v12H8V12z'/></svg>",
+  },{
+    "name": "hexagons",
+  "height": "49",
+   "width": "28",
+     "svg": "<svg><path fill='#000000' fill-rule='nonzero' d='M13.99 9.25l13 7.5v15l-13 7.5L1 31.75v-15l12.99-7.5zM3 17.9v12.7l10.99 6.34 11-6.35V17.9l-11-6.34L3 17.9zM0 15l12.98-7.5V0h-2v6.35L0 12.69v2.3zm0 18.5L12.98 41v8h-2v-6.85L0 35.81v-2.3zM15 0v7.5L27.99 15H28v-2.31h-.01L17 6.35V0h-2zm0 49v-8l12.99-7.5H28v2.31h-.01L17 42.15V49h-2z'/></svg>",
+  },{
+    "name": "hideout",
+  "height": "40",
+   "width": "40",
+     "svg": "<svg viewBox='0 0 40 40'><path fill='#000000' d='M0 38.59l2.83-2.83 1.41 1.41L1.41 40H0v-1.41zM0 1.4l2.83 2.83 1.41-1.41L1.41 0H0v1.41zM38.59 40l-2.83-2.83 1.41-1.41L40 38.59V40h-1.41zM40 1.41l-2.83 2.83-1.41-1.41L38.59 0H40v1.41zM20 18.6l2.83-2.83 1.41 1.41L21.41 20l2.83 2.83-1.41 1.41L20 21.41l-2.83 2.83-1.41-1.41L18.59 20l-2.83-2.83 1.41-1.41L20 18.59z'/></svg>",
+  },{
     "name": "plus",
   "height": "60",
    "width": "60",
@@ -481,7 +496,7 @@ const PATTERNS_SOURCE = [
     "name": "polka-dots",
   "height": "20",
    "width": "20",
-     "svg": "<svg><g fill='#000000'><circle id='Oval-377-Copy-9' cx='3' cy='3' r='3'></circle><circle cx='13' cy='13' r='3'></circle></g></svg>",
+     "svg": "<svg><circle fill='#000000' cx='3' cy='3' r='3'></circle><circle cx='13' cy='13' r='3'></circle></svg>",
   },{
     "name": "rails",
   "height": "10",
